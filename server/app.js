fc1bf2a59883b4bd0f513898c3acd653dadb890b
Akio <aakocherga@mail.ru>
2025-06-17 19:45:17 +0700

    Added cors, new port from client

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const indexRouter = require("./routes/indexRouter");
 
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.use("/", indexRouter);
 
