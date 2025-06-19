9389a51141cb0e29d6a1f78be4a75cf08b52cfe3
Akio Kane <aakocherga@mail.ru>
2025-06-19 14:28:48 +0700

    Added header user info

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,67 @@
-import "../styles/Header.css";
+import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
+import user from "../assets/user.svg";
+import "../styles/Header.css";
 
 function Header() {
+  const [userInfoDisplay, setUserInfoDisplay] = useState("none");
+  const userInfoRef = useRef(null);
+  const userImgRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (userInfoRef.current && 
+        !userInfoRef.current.contains(event.target) &&
+        !userImgRef.current.contains(event.target)
+      ) {
+        setUserInfoDisplay("none")
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const profile = (name) => {
+    return (
+      <>
+        <div className="profile">
+          <img ref={userImgRef} src={user} alt="user" onMouseDown={() => {
+            setUserInfoDisplay("flex");
+          }} />
+          <div ref={userInfoRef} className="user-info" style={{
+            display: userInfoDisplay
+          }}>
+            {/* User Menu */}
+          </div>
+        </div>
+      </>
+    )
+  }
+
+  const loginBtns = () => {
+    return (
+      <>
+        <div className="login-btns">
+          <button className="login">Sign In</button>
+          <button className="register">Sign Up</button>
+        </div>
+      </>
+    )
+  }
+
+  const checkRegister = () => {
+    const register = false; // cookie flag
+    if (register) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   return (
     <>
       <header className="header-main">
@@ -20,7 +80,9 @@ function Header() {
           </Link>
         </div>
 
-        <div className="user-elements"></div>
+        <div className="user-elements">
+          {checkRegister() ? profile("akio") : loginBtns()}
+        </div>
       </header>
     </>
   )
