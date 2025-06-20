d9cd727c9a211b7682edb7ff672cae95d055e075
Akio <aakocherga@mail.ru>
2025-06-20 22:38:40 +0700

    added home page

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,13 +1,19 @@
 import "../styles/Home.css";
-import cloudStorage from "../assets/cloud_storage.svg"
+import cloudStorage from "../assets/cloud_storage.svg";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <>
-      {/* <img src={cloudStorage} alt="Cloud Storage" /> */}
-
       <div className="home">
-        
+        <div className="home-source">
+          <h1>Lorem ipsum dolor sit amet consectetur dolores ex reiciendis.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. possimus amet at obcaecati fugit deleniti ipsum!</p>
+          <Link>
+            <button className="start">Get Started!</button>
+          </Link>
+        </div>
+        <img src={cloudStorage} alt="Cloud Storage" />
       </div>
     </>
   )
