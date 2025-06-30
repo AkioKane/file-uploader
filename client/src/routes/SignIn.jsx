8de346eb61abaff7ca18209126c768271fe49b0d
Akio <aakocherga@mail.ru>
2025-07-01 00:16:53 +0700

    Added styles sign components

--- client/src/routes/SignIn.jsx
+++ client/src/routes/SignIn.jsx
@@ -1,9 +1,73 @@
+import { useState } from "react";
 import "../styles/SignIn.css";
 
+async function postSignIn(data) {
+  try {
+    const response = await fetch('/api/sign-in', {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const result = await response.json();
+    console.log("Result", result);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+
 function SignIn() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleOnSumbit = (e) => {
+    e.preventDefault();
+
+    postSignIn(formData)
+  }
+
+  const handlOnChange = (e) => {
+    setFormData({
+      ...formData, 
+      [e.target.name]: e.target.value
+    });
+  }
+
   return (
     <>
-      <div className="sign-in"></div>
+      <div className="sign-in">
+        <h1>Sign In</h1>
+        <form 
+          action="POST"
+          onSubmit={handleOnSumbit}
+        >
+          <label htmlFor="email">Email*:</label>
+          <input 
+            id="email"
+            type="email" 
+            name="email"
+            value={formData.email}
+            onChange={handlOnChange} 
+            placeholder="Email"
+            required
+          />
+          <label htmlFor="password">Password*:</label>
+          <input 
+            id="password"
+            type="password" 
+            name="password"
+            value={formData.password}
+            onChange={handlOnChange} 
+            placeholder="Password"
+            required
+          />
+          <button className="btn-sumbit" type="submit">Sumbit</button>
+        </form>
+      </div>
     </>
   );
 }
--- client/src/routes/SignUp.jsx
+++ client/src/routes/SignUp.jsx
@@ -1,9 +1,83 @@
+import { useState } from "react";
 import "../styles/SignUp.css";
 
+async function postSignUp(data) {
+  try {
+    const response = await fetch('/api/sign-in', {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const result = await response.json();
+    console.log("Result", result);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
 function SignUp() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
+
+  const handleOnSumbit = (e) => {
+    e.preventDefault();
+
+    postSignUp(formData)
+  }
+
+  const handlOnChange = (e) => {
+    setFormData({
+      ...formData, 
+      [e.target.name]: e.target.value
+    });
+  }
+
   return (
     <>
-      <div className="sign-up"></div>
+      <div className="sign-up">
+        <h1>Sign Up</h1>
+        <form 
+          action="POST"
+          onSubmit={handleOnSumbit}
+        >
+          <label htmlFor="name">Name*:</label>
+          <input 
+            id="name"
+            type="text" 
+            name="name"
+            placeholder="Name"
+            value={formData.name}
+            onChange={handlOnChange} 
+            required
+          />
+          <label htmlFor="email">Email*:</label>
+          <input 
+            id="email"
+            type="email" 
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handlOnChange} 
+            required
+          />
+          <label htmlFor="password">Password*:</label>
+          <input 
+            id="password"
+            type="text" 
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handlOnChange} 
+            required
+          />
+          <button className="btn-sumbit" type="submit">Sumbit</button>
+        </form>
+      </div>
     </>
   );
 }
